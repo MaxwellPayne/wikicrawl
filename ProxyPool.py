3ff29ccef9057b56963e79fce641895829d28a47
MaxwellPayne <mpmclenn@indiana.edu>
2014-07-09 03:30:52 -0300

    Working on the ProxyPool

--- ProxyPool.py
+++ ProxyPool.py
@@ -3,6 +3,7 @@ import time
 
 
 from Queue import Queue
+from threading import Thread
 
 class ProxyProtocol:
     http = 'http'
@@ -11,14 +12,47 @@ class ProxyProtocol:
 
 class ProxyPool(object):
     VERIFY_URL = 'http://icanhazip.com'
+    INIT_WAIT_TIME = 3
     
     def __init__(self, IPFiles, pool_size=None, test_buffer_size=20):
         self.files = IPFiles if isinstance(IPFiles, list) else [IPFiles]
-        self.untested = Queue(maxsize=buffer_size)
-        self.proxies = Queue(maxsize=pool_size)
+        self._untested = Queue(maxsize=test_buffer_size)
+        self._proxies = Queue(maxsize=pool_size)
+        self.openFile = open(self.files.pop(0), 'r')
+
+        """Init refilling thread"""
+        def testAndPut(ip):
+            def callback():
+                print 'try refil %s' % ip
+                if proxyIsValid(ip):
+                    self.untested.put(ip)
+                return callback
+        
+        def masterRefillThread():
+            print 'THE MASTER LIVES'
+            while True:
+                if not self._untested.full():
+                    newIP = self._readIP()
+                    
+                    filler = Thread(target=testAndPut(newIP))
+                    filler.setDaemon(True)
+                    filler.start()
+                time.sleep(0.01)
+
+        masterRefiller = Thread(target=masterRefillThread)
+        masterRefiller.setDaemon(True)
+        masterRefiller.start()
+
+        #time.sleep(self.INIT_WAIT_TIME)
+
+    def __del__(self):
+        print '__del__'
+        if self.openFile and not self.openFile.closed():
+            self.openFile.close()
 
     @staticmethod
     def isIPv4(ipStr):
+        print 'isIPv4'
         try:
             byteArray = map(int, ipStr.split('.'))
             byteTest = [byte for byte in byteArray if byte >= 0 and byte <= 255]
@@ -28,6 +62,7 @@ class ProxyPool(object):
 
     @classmethod
     def proxyIsValid(cls, ip):
+        print 'proxyIsValid'
         proxyDict = {protocol: ip for protocol in ('http', 'https', 'ftp')}
         try:
             r = requests.get(cls.VERIFY_URL, proxies=proxyDict)
@@ -36,14 +71,29 @@ class ProxyPool(object):
         except requests.HTTPError:
             return False
 
+    def _readIP(self):
+        print 'readIP'
+        try:
+            return self.openFile.readline().rstrip()
+        except EOFError:
+            self.openFile.close()
+            if not self.files:
+                raise EOFError("No more IP Files")
+            self.openFile = open(self.files.pop(0), 'r')
+            return openFile.readline().rstrip()
+
     def putBack(self, ip, delay=0):
+        print 'putBack'
         if self.isIPv4(ip):
             time.sleep(delay)
             self.proxies.put(ip)
         else: 
             raise ValueError("%s not a valid IPv4" % ip)
 
-        
+    def get(self, block=True, timeout=None):
+        print 'get'
+        return self._proxies.get(block=block, timeout=timeout)
+
 
             
         
